4d2d3bd9b9fae9371daa70a5813db1da9bd6f426
Emanuel Saraiva <152085889+EmanuelPCS@users.noreply.github.com>
2023-12-01 05:30:56 +0000

    Update bd.sql

--- bd.sql
+++ bd.sql
@@ -2,6 +2,7 @@
 /* DBMS name:      ORACLE Version 11g                           */
 /* Created on:     12/11/2022 2:08:37 PM                        */
 /*==============================================================*/
+-- Arquivo SQL para o banco de dados
 
 
 alter table AVIOES
